5d6db9368ff956e80b63cf078b8d04f260329ead
Sesi <sesi@escolasesiba.com.br>
2024-07-23 18:06:45 -0300

    terceira versão

--- src/JS/Navbar.js
+++ src/JS/Navbar.js
@@ -1,22 +1,10 @@
 import React, { useState } from 'react';
 import '../CSS/Navbar.css';
-import Menuzin from './Menuzin';
 
 const Navbar = () => {
-  const [sidebar, setSidebar] = useState(false);
-
-  const toggleSidebar = () => {
-    setSidebar(!sidebar);
-  };
-
   return (
     <div className="navbar">
       <div className="navbar-left">
-        <button className="hamburger" onClick={toggleSidebar}>
-          <div className="bar"></div>
-          <div className="bar"></div>
-          <div className="bar"></div>
-        </button>
         <a href="#home" className="nav-link">Início</a>
         <a href="#contact" className="nav-link">Contato</a>
       </div>
@@ -25,7 +13,6 @@ const Navbar = () => {
           <input type="text" placeholder="Pesquisar..." className="search-bar" />
         </div>
       </div>
-      {sidebar && <Menuzin closeSidebar={toggleSidebar} />}
     </div>
   );
 };
