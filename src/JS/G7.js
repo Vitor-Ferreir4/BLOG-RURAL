fe491e06098b2c99b6dd8df0ea89487fdd6d8f0b
Vitor-Ferreir4 <166330954+Vitor-Ferreir4@users.noreply.github.com>
2024-08-01 15:37:05 -0300

    Update G7.js

--- src/JS/G7.js
+++ src/JS/G7.js
@@ -1,10 +1,10 @@
 import React from "react"
-import '../CSS/G7'
+import '../CSS/G7.css'
 
 
 function G7(){
     return(
-        <div className="G1-container">
+        <div className="G7-container">
             <main className="conteudo">
                 <h1>COLOQUE SEU CONTEÚDO AQUI E NÃO ALTERE A NAVBAR NEM O FOOTER!!!</h1>
             </main>
