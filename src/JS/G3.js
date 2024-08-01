336f31cd73153d8cf8f5e98fe514838929662650
Vitor-Ferreir4 <166330954+Vitor-Ferreir4@users.noreply.github.com>
2024-08-01 15:34:12 -0300

    Update G3.js

--- src/JS/G3.js
+++ src/JS/G3.js
@@ -4,7 +4,7 @@ import '../CSS/G3.css'
 
 function G3(){
     return(
-        <div className="G1-container">
+        <div className="G3-container">
             <main className="conteudo">
                 <h1>COLOQUE SEU CONTEÚDO AQUI E NÃO ALTERE A NAVBAR NEM O FOOTER!!!</h1>
             </main>
