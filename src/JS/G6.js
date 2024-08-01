005943ec2356ef339b60072f683bb3b48fcd499b
Vitor-Ferreir4 <166330954+Vitor-Ferreir4@users.noreply.github.com>
2024-08-01 15:36:07 -0300

    Update G6.js

--- src/JS/G6.js
+++ src/JS/G6.js
@@ -1,10 +1,10 @@
 import React from "react"
-import '../CSS/G6'
+import '../CSS/G6.css'
 
 
 function G6(){
     return(
-        <div className="G1-container">
+        <div className="G6-container">
             <main className="conteudo">
                 <h1>COLOQUE SEU CONTEÚDO AQUI E NÃO ALTERE A NAVBAR NEM O FOOTER!!!</h1>
             </main>
