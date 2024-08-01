b424d7bbcb55d680dc5c0549526008ef1fa08f81
Vitor-Ferreir4 <166330954+Vitor-Ferreir4@users.noreply.github.com>
2024-08-01 15:35:09 -0300

    Update G4.js

--- src/JS/G4.js
+++ src/JS/G4.js
@@ -1,10 +1,10 @@
 import React from "react"
-import '../CSS/G4'
+import '../CSS/G4.css'
 
 
 function G4(){
     return(
-        <div className="G1-container">
+        <div className="G4-container">
             <main className="conteudo">
                 <h1>COLOQUE SEU CONTEÚDO AQUI E NÃO ALTERE A NAVBAR NEM O FOOTER!!!</h1>
             </main>
